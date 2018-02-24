1260b46e88f553549008da89a7794157bae7cb90
Martin Ivanov <martin.ivanov@accedia.com>
2018-02-24 16:14:54 +0200

    refactor function

--- function.js
+++ function.js
@@ -1,12 +1,5 @@
-function testFunction (pattern) {
-  let myarr = [];
-  myArr.push(1);
-  myArr.push(2);
-  myArr.push(3);
-
-  let filteredArr = myArr.filter((elem) => {
+function testFunction (source, pattern) {
+  return source.filter((elem) => {
     return elem === pattern;
   });
-  
-  retrun myarr;
 }
