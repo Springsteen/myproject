1c2b111902233532ce32c4a2788cc0e7cc66476b
Martin Ivanov <martin.ivanov@accedia.com>
2018-02-24 16:09:59 +0200

    find in array

---
+++ function.js
@@ -0,0 +1,12 @@
+function testFunction (pattern) {
+  let myarr = [];
+  myArr.push(1);
+  myArr.push(2);
+  myArr.push(3);
+
+  let filteredArr = myArr.filter((elem) => {
+    return elem === pattern;
+  });
+  
+  retrun myarr;
+}
